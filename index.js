908be2f654fd9332466764a6021fafa4199c633f
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-27 07:23:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function addComment(){
-  var bodyText = "This research is bold and important!"
-  var commenter = "Definitely not a woodchuck looking to eat the wood"
+  var bodyText = document.getElementById("commentText").value
+  var commenter = document.getElementById("commenterName").value
 
   var commentBody = createCommentBody(bodyText)
   var commenterLabel = createCommenterLabel(commenter)
@@ -13,4 +13,23 @@ function createCommentBody(comment){
   bodyPara.innerHTML = comment
   bodyDiv.appendChild(bodyPara)
   return bodyDiv
+}
+
+function createCommenterLabel(commenter){
+  var commenterDiv = document.createElement("div");
+  var commenterLabel = document.createElement("p");
+  commenterLabel.innerHTML = "posted by:&nbsp;";
+  var commenterName = document.createElement('span')
+  commenterName.innerHTML = commenter
+  commenterLabel.appendChild(commenterName)
+  commenterDiv.appendChild(commenterLabel)
+  return commenterLabel
+}
+
+function postNewComment(body, commenter){
+  var commentsDiv = document.getElementById("comments");
+  var newCommentDiv = document.createElement("div");
+  newCommentDiv.appendChild(body)
+  newCommentDiv.appendChild(commenter)
+  commentsDiv.appendChild(newCommentDiv)
 }
